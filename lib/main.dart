547914c55db2993b49845065aaae00c73b6652a0
s1aro0 <sara021wo@gmail.com>
2026-04-19 23:12:52 +0300

    fix: load .env for video (Agora) and add Upload SVG icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'app/app_theme.dart';
@@ -22,6 +23,9 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load .env file so dotenv.env[...] works throughout the app
+  await dotenv.load(fileName: '.env');
 
   try {
     await Firebase.initializeApp(
